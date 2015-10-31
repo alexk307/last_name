ec0e7b3141441be5dc017bb902a5a71078023486
Alex Kahan <alex@zerofox.com>
2015-10-31 15:41:52 -0400

    Cleaning up and versioning up

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='name_suggestor',
     packages=['name_suggestor'],
-    version='0.1.4',
+    version='0.2.0',
     description='Suggests a name based on other names found in the US census',
     author='Alex Kahan',
     author_email='alexk307@gmail.com',
