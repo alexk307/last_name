1374e44640bb06be7a33cd0f8a822245104704a8
Justin Black <jblack@orionlabs.io>
2018-04-13 16:32:12 -0700

    Adds relative import of name_file in name.py to fix ModuleNotFoundError in terminal

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='name_suggestor',
     packages=['name_suggestor'],
-    version='0.2.0',
+    version='0.2.1',
     description='Suggests a name based on other names found in the US census',
     author='Alex Kahan',
     author_email='alexk307@gmail.com',
